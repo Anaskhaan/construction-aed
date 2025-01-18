798aab40d5c7743b803a6cb04135f3c8fc196909
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2025-01-18 13:29:35 +0500

    pages created and routes configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import Terms from "./pages/Terms";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import GetQuote from "./pages/GetQuote";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello world!
-      </h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Blogs" element={<Blogs />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Terms" element={<Terms />} />
+          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+          <Route path="/GetQuote" element={<GetQuote />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <div>Home</div>
+      <p>Welcome to the home page of your app!</p>
+    </>
+  );
+}
