8b48cffb00efdd94b7057930ae08559ee4327289
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2025-01-20 05:24:30 -0800

    Quote-Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -9,24 +8,28 @@ import Contact from "./pages/Contact";
 import Terms from "./pages/Terms";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import GetQuote from "./pages/GetQuote";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Blogs" element={<Blogs />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Terms" element={<Terms />} />
-          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-          <Route path="/GetQuote" element={<GetQuote />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Blogs" element={<Blogs />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Terms" element={<Terms />} />
+            <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+            <Route path="/GetQuote" element={<GetQuote />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+      <Toaster />
+    </>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
-    <>
-      <div>Home</div>
-      <p>Welcome to the home page of your app!</p>
-    </>
+    <div className="relative flex h-screen w-screen">
+      {/* Left Image with Blurry Right Edge */}
+      <div className="h-full w-1/2 relative">
+        <img
+          src="/Hero1Image.png"
+          alt="whitepic"
+          className="h-full w-full object-cover"
+        />
+        {/* Blurry Overlay on Right Edge */}
+        <div className="absolute top-0 right-0 h-full w-16 bg-gradient-to-l from-white to-transparent pointer-events-none"></div>
+      </div>
+
+      {/* Right Image */}
+      <img
+        src="/HeroImage.png"
+        alt="hero-image"
+        className="h-full w-1/2 object-cover -ml-16" /* Negative margin ensures overlap */
+      />
+    </div>
   );
 }
