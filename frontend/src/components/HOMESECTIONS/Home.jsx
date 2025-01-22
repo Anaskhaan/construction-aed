cfa64f900f520cb9553434cb20d122fb94f33b21
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2025-01-22 08:04:17 -0800

    about-done

---
+++ frontend/src/components/HOMESECTIONS/Home.jsx
@@ -0,0 +1,29 @@
+import About from "../About";
+import Hero from "./Hero";
+import BlogSection from "./BlogSection";
+import FAQs from "./FAQs";
+import HomepageServices from "./HomepageServices";
+
+import TradesPreview from "../HOMESECTIONS/TradesPreview";
+
+import ConstructionBanner from "./ConstrutionBanner";
+import Quote from "../HOMESECTIONS/Quote";
+import WhyHire from "../HOMESECTIONS/WhyHire";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <div className=" px-16">
+        <HomepageServices />
+        <ConstructionBanner />
+        <WhyHire />
+        <TradesPreview />
+        <Quote />
+        <FAQs />
+        <BlogSection />
+      </div>
+    </>
+  );
+}
