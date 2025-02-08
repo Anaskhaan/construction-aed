31fa97c50794534f510cd56a7a4f71c1fdf1daef
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2025-02-08 08:45:22 -0800

    done

--- frontend/src/Header/Sidebar.jsx
+++ frontend/src/Header/Sidebar.jsx
@@ -24,61 +24,65 @@ const Sidebar = ({
         <X size={24} />
       </button>
 
-      <div className="mt-16 p-4 space-y-6">
-        <Link to="/" className="block py-2 hover:text-[#0163BE]">
-          Home
-        </Link>
-        <Link to="/about" className="block py-2 hover:text-[#0163BE]">
-          About Us
-        </Link>
+      <div className="h-full flex flex-col">
+        <div className="mt-16 p-4 space-y-6 flex-grow overflow-y-auto">
+          <Link to="/" className="block py-2 hover:text-[#0163BE]">
+            Home
+          </Link>
+          <Link to="/about" className="block py-2 hover:text-[#0163BE]">
+            About Us
+          </Link>
 
-        <div className="relative">
-          <button
-            className="flex gap-1 items-center py-2 w-full text-left hover:text-[#0163BE]"
-            onClick={() => setServicesDropdown(!servicesDropdown)} // Toggle on click
-          >
-            Our Services <ChevronDown />
-          </button>
-          {servicesDropdown && (
-            <div className="space-y-2 pl-4">
-              {services.map((service, index) => (
-                <Link
-                  key={index}
-                  to={service.path}
-                  className="block py-1 hover:bg-blue-100"
-                >
-                  {service.name}
-                </Link>
-              ))}
-            </div>
-          )}
+          {/* Services Dropdown */}
+          <div className="relative">
+            <button
+              className="flex gap-1 items-center py-2 w-full text-left hover:text-[#0163BE]"
+              onClick={() => setServicesDropdown(!servicesDropdown)} // Toggle on click
+            >
+              Our Services <ChevronDown />
+            </button>
+            {servicesDropdown && (
+              <div className="space-y-2 pl-4">
+                {services.map((service, index) => (
+                  <Link
+                    key={index}
+                    to={service.path}
+                    className="block py-1 hover:bg-blue-100"
+                  >
+                    {service.name}
+                  </Link>
+                ))}
+              </div>
+            )}
+          </div>
+
+          {/* Trades Dropdown */}
+          <div className="relative">
+            <button
+              className="flex gap-1 items-center py-2 w-full text-left hover:text-[#0163BE]"
+              onClick={() => setTradesDropdown(!tradesDropdown)} // Toggle on click
+            >
+              Trades <ChevronDown />
+            </button>
+            {tradesDropdown && (
+              <div className="space-y-2 pl-4">
+                {trades.map((trade, index) => (
+                  <Link
+                    key={index}
+                    to={trade.path}
+                    className="block py-1 hover:bg-blue-100"
+                  >
+                    {trade.name}
+                  </Link>
+                ))}
+              </div>
+            )}
+          </div>
+
+          <Link to="/contact" className="block py-2 hover:text-[#0163BE]">
+            Contact Us
+          </Link>
         </div>
-
-        <div className="relative">
-          <button
-            className="flex gap-1 items-center py-2 w-full text-left hover:text-[#0163BE]"
-            onClick={() => setTradesDropdown(!tradesDropdown)} // Toggle on click
-          >
-            Trades <ChevronDown />
-          </button>
-          {tradesDropdown && (
-            <div className="space-y-2 pl-4">
-              {trades.map((trade, index) => (
-                <Link
-                  key={index}
-                  to={trade.path}
-                  className="block py-1 hover:bg-blue-100"
-                >
-                  {trade.name}
-                </Link>
-              ))}
-            </div>
-          )}
-        </div>
-
-        <Link to="/contact" className="block py-2 hover:text-[#0163BE]">
-          Contact Us
-        </Link>
       </div>
     </div>
   );
